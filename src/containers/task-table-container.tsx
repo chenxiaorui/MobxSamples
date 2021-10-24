219567ac897a210d2c2a76897a73284c2a87c5c7
colin chen <colinchen@outlook.com>
2021-10-24 21:57:08 +0800

    install typescript and init taskTableContainer

---
+++ src/containers/task-table-container.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface Props {
+    tableName: string
+}
+
+class TaskTableContainer extends React.Component<Props> {
+    render() {
+        return (
+            <div>
+                'hello world'
+            </div>
+        )
+    }
+}
+
+export { TaskTableContainer };
